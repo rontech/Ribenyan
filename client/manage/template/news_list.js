f7e6ea37ad0daab0ffce96df95fbad2addd3a725
tonybing <haibing-huang@sz-rontech.com>
2016-02-25 14:27:41 +0800

    新闻信息整合表一览画面修改

--- client/manage/template/news_list.js
+++ client/manage/template/news_list.js
@@ -3,3 +3,10 @@ $('.datatable').dataTable({"sPaginationType": "bs_normal"});
 $('.datatable').dataTable({"sPaginationType": "bs_two_button"});
 $('.datatable').dataTable({"sPaginationType": "bs_four_button"});
 $('.datatable').dataTable({"sPaginationType": "bs_full"});
+
+Template.newsList.helpers({
+
+  newslists: function() {
+    return NewsInfo.find();
+  },  
+});
