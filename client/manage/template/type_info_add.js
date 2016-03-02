73455d55da04cf52e76b309782218e6149e68201
deric0314 <deric_c@outlook.com>
2016-03-02 18:37:11 +0800

    新闻类型add功能

---
+++ client/manage/template/type_info_add.js
@@ -0,0 +1,18 @@
+Template.typeInfoAdd.events({
+    'submit #type_info_add' : function(e,t){
+        e.preventDefault();
+        var parentID = t.find('#parentID').value;
+        var name     = t.find('#name').value;
+        var isVaild  = t.find('#isVaild').value;
+        var remark   = t.find('#remark').value;
+
+        TypeInfo.insert({
+        				"parentID":parentID,
+            			"name":name,
+        				"isVaild":isVaild,
+        				"remark":remark,
+        				},function(){
+                                alert("已保存");
+                        });
+    },
+});
