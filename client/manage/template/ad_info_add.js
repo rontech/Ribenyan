7301fb372d546479612c496f020c3386718bac11
deric0314 <deric_c@outlook.com>
2016-03-02 18:27:13 +0800

    标签主表 add功能

--- client/manage/template/ad_info_add.js
+++ client/manage/template/ad_info_add.js
@@ -15,6 +15,6 @@
             			"introduce":introduce,
         				"content":content,
         				"cstId":cstId,
-        				}),function(){alert("已保存");};
+        				},function(){alert("已保存");});
     },
 });
---
+++ client/manage/template/tag_info_add.js
@@ -0,0 +1,20 @@
+Template.tagInfoAdd.events({
+    'submit #tag_info_add' : function(e,t){
+        e.preventDefault();
+        var type    = t.find('#type').value;
+        var name    = t.find('#name').value;
+        var isVaild = t.find('#isVaild').value;
+        var note    = t.find('#note').value;
+        var tagIcon = t.find('#tagIcon').value;
+        console.log(type,name,isVaild,note,tagIcon);
+        TagInfo.insert({
+        				"type":type,
+            			"name":name,
+        				"isVaild":isVaild,
+        				"note":note,
+                        "tagIcon":tagIcon
+        				},function(){
+                                alert("已保存");
+                        });
+    },
+});
