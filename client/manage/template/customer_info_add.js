7fb135a6a1f42ef4a9fe39b664722e3b5cef1a24
deric0314 <deric_c@outlook.com>
2016-03-24 16:09:19 +0800

    后台用户 登入界面（密码md5）

---
+++ client/manage/template/customer_info_add.js
@@ -0,0 +1,33 @@
+Template.manageUserAdd.events({
+    'submit #manageuseradd-form' : function(e,t){
+        var createtime = new Date().Format("yyyy/MM/dd/hh:mm:ss");
+        e.preventDefault();
+        var username = t.find('#username').value;
+        var pwd = t.find('#pwd').value;
+        var pwdq = t.find('#pwdq').value;
+        var email = t.find('#email').value;
+        var tel = t.find('#tel').value;
+        var newsperms = t.find('#newsperms').value;
+        var adperms = t.find('#adperms').value;
+        var ctrlperms = t.find('#ctrlperms').value;
+
+        if(pwd==pwdq){
+            pwd = $.md5(pwd); 
+            AdminInfo.insert({
+            "username":username,
+            "pwd":pwd,
+            "email":email,
+            "tel":tel,
+            "newsperms":newsperms,
+            "adperms":adperms,
+            "ctrlperms":ctrlperms,
+            "createTime":createtime
+            },function(){
+                alert("录入成功");
+                Router.go("/manage/newslist");
+             });
+        }else{
+            alert("密码输入不一致，请重新输入！");
+        }
+    },
+});
