ca1568421b81fe5c31efa8a0a3063f16ca211a68
deric0314 <deric_c@outlook.com>
2016-03-24 13:26:19 +0800

    广告 新闻 一览表 删除方式变更

--- client/manage/template/news/news_list.js
+++ client/manage/template/news/news_list.js
@@ -87,7 +87,7 @@ Template.featureComparison.helpers({
           sortable: false,
           headerClass: 'span1',
           fn: function (name,object) {
-           var html = '<div class="text-right"><a class="btn btn-info" href="/manage/newslist/' + object._id + '">编辑</a><a class="btn btn-danger" href="/manage/newsdel/' + object._id + '">删除</a></div>';
+           var html = '<div class="text-right"><a class="btn btn-info" href="/manage/newslist/' + object._id + '">编辑</a><button name="delete" class="btn btn-danger" value="' + object._id  + '">删除</button></div>';
             return new Spacebars.SafeString(html);
           }
         }
@@ -95,3 +95,20 @@ Template.featureComparison.helpers({
     };
   }
 });
+
+Template.featureComparison.events({
+        'click [name=delete]': function (ev) {
+          ev.preventDefault();
+          var del = window.confirm('该条信息删除！')
+        if(del==true){            
+              var id = ev.currentTarget.value;
+              
+              var newsdata = NewsInfo.findOne({_id:new Meteor.Collection.ObjectID(id)});
+              var imageObj = newsdata.imageObj;
+          for(var i=0;i<imageObj.length;i++){
+            Files.remove(imageObj[i]);
+        }
+              NewsInfo.remove({_id:new Meteor.Collection.ObjectID(id)});
+            }
+        }
+    });
