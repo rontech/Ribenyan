bb5cd2acac22cd80ca2d5e0f0029e7bd6aa2af97
deric0314 <deric_c@outlook.com>
2016-03-31 14:43:37 +0800

    版块权限 bug修正

--- client/manage/template/news/news_type/type_obj_show.js
+++ client/manage/template/news/news_type/type_obj_show.js
@@ -1,7 +1,7 @@
 Template.typeShow.created = function() {   
 	ckPerms('newsperms');
 }
-
+//废弃
 Template.typeShow.helpers({
 	typelists: function() {
 		return TypeInfo.find({isVaild:1}).fetch();
@@ -19,7 +19,7 @@ Template.typeShow.helpers({
 		return "";
 	}
 });
-
+//news add
 Template.userTypeShow.helpers({
 	typelists: function() {
 		var user = sessionStorage.getItem('login_user');
@@ -32,16 +32,24 @@ Template.userTypeShow.helpers({
 		console.log(id);
 		return TypeInfo.find({'_id':{$in:id},isVaild:1}).fetch(); 
 	},
+	
 });
 
-Template.userTypeShow.helpers({
-		checked: function() {
-		var types = Session.get("news_info").typeObj
-		for(var i = 0;i<types.length;i++){
-			if(this._id._str === types[i].typeID._str) {
+//user add
+Template.typeShowAll.helpers({
+	typelists: function() {
+		return TypeInfo.find({isVaild:1}).fetch();
+	},
+	checked: function() {
+		var user = Session.get('user_info');
+		console.log("////////"+user.newsblockperms);
+		var id = new Array();
+		for(var i=0;i<user.newsblockperms.length;i++){
+			if(this._id._str === user.newsblockperms[i].typeID._str) {
 				return "checked";
 			}
 		}
 		return "";
 	}
 });
+
