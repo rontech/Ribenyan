adf47b54406a55a51f6f6ed5ee0bbd98ae228e9c
shijian.yang <shijian.yangsd@gmail.com>
2016-03-31 12:02:06 +0900

    查询Ｂｕｇ修复

--- client/dashBoard/search/search_result.js
+++ client/dashBoard/search/search_result.js
@@ -2,25 +2,29 @@ var advNum = 2;
 
 Template.searchResult.events({
 	"click button.js-search" : function(e){//点击查询
-		var inputObj = $("#search-input");
+		var inputObj = $("#search-input1");
 		var searchText = inputObj.val();
 		if(isEmpty(searchText)){
 			alert(SEARCH_TEXT_NOT_NULL);
 			return false;
 		}else{
 			// 打开新窗口
-			$("#searchForm").submit();
+			$("#searchForm1").submit();
 		}
 	}
 });
 
 Template.searchResult.helpers({
+	"searchText" : function(){
+		return Template.currentData().searchText;
+	},
 	"isHavaResult":function(){
 		if(compositeData().length>0){
 			return true;
 		}else{
 			return false;
 		}
+		// return true;
 	},
 	"datalist" : function(){
 		console.log(Template.currentData().searchText);
@@ -28,9 +32,7 @@ Template.searchResult.helpers({
 			return compositeData();
 		}
 	},
-	"searchText" : function(){
-		return Template.currentData().searchText;
-	}
+	
 });
 
 // 组合数据
@@ -39,14 +41,23 @@ function compositeData(){
 	var searchText = new RegExp(Template.currentData().searchText);
 
 	var newsInfo = searchNewsData(searchText).fetch();
-	var advInfo = searchAdverInfo(searchText).fetch();
 
-	console.log(newsInfo);
+	var advInfo = searchAdverInfo(searchText);
 
-	if(!newsInfo){
-		return advInfo;
+
+	if(newsInfo.length == 0){
+		var listInfo = new Array();
+		var j = 0;
+
+		//混合数据
+		for(var i = 0;i < advInfo.length;i++){
+			advInfo[i].isAdv = true; 
+			listInfo.push(advInfo[i]);
+			j++;	
+		}
+		return listInfo;
 	}
-	if(!advInfo){
+	if(advInfo.length == 0){
 		return newsInfo;
 	}
 
@@ -89,8 +100,30 @@ function searchNewsData(searchText){
 //广告数据
 function searchAdverInfo(searchText){
 
-	return SetingAdvInfo.find({
-								"isVaild" : 1,
-								"title" : searchText
-							});
+	// 查询广告
+	var advList = AdInfo.find({
+								$or :[
+										{"title" : searchText},
+										{"address" : searchText},
+										{"introduce" : searchText},
+										{"content" : searchText}
+									]
+								}
+							).fetch();
+	console.log(advList);
+
+	if(advList.length > 0 ){
+		var list = new Array();
+
+		for(var i = 0;i<advList.length;i++){
+
+			var advInfo = SetingAdvInfo.findOne({"advID":advList[i]._id});
+			if(advInfo){
+				list.push(advInfo);
+			}
+		}
+		return list;
+	}else{
+		return new Array();
+	}
 }
--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 Meteor.subscribe("severPublicName","tom");
 Meteor.subscribe("layout");
 Meteor.subscribe("header");
-Meteor.subscribe("bus_news_info");
+
 Meteor.subscribe("bus_advertisement_info");
+Meteor.subscribe("bus_news_info");
+
 Meteor.subscribe("bus_second_layout_data_info");
 Meteor.subscribe("bus_news_evaluation_info");
 Meteor.subscribe("bus_type_info");
