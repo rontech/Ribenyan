7d989d52abc1174237aef8f2c76ebbde97739dd8
deric0314 <deric_c@outlook.com>
2016-04-05 14:51:22 +0800

    no message

--- client/application/layout/manage_layout.js
+++
@@ -1,9 +0,0 @@
-Template.manageLayout.newsPermission = function (q) {
-  return sessionStorage.getItem('newsperms') === q;
-} ;
-Template.manageLayout.adPermission = function (q) {
-  return sessionStorage.getItem('adperms') === q;
-} ;
-Template.manageLayout.ctrlPermission = function (q) {
-  return sessionStorage.getItem('ctrlperms') === q;
-} ;
