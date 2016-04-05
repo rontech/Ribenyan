e7c63f633a31e27eb240b869b94447ecf071e22c
deric0314 <deric_c@outlook.com>
2016-04-05 12:48:50 +0800

    no message

--- client/manage/template/manage_user/manage_user_list.js
+++ client/manage/template/manage_user/manage_user_list.js
@@ -3,14 +3,16 @@ Template.manageUserList.created = function() {
 	ckPerms('ctrlperms');
 }
 
+
 //通用显示
-varCommon = function (value) {
+var Common = function (value) {
 	var html;
 	// first, normalize the value to a canonical interpretation
 	if (typeof value === 'boolean')
 		value = {
-			support: value
+		support: value
 		};
+
 	if (value === null || value === undefined || value ==="") {
 		html = '<span style="color: orange; font-weight: bold">待输入</span>';
 	} else {
@@ -18,6 +20,7 @@ varCommon = function (value) {
 	}
 	return new Spacebars.SafeString(html);
 };
+
 
 Template.manageListTable.helpers({
 	tables : function () {
@@ -29,26 +32,30 @@ Template.manageListTable.helpers({
 			rowsPerPage: 10,
 			showNavigation: 'auto',
 			showColumnToggles: false,
-			fields: 
-			[
-				{
-					key: 'username',
-					label: '账号',
-					headerClass: '',
-					cellClass:'',
-					fn: function (name,object) {var html = '<a href="/manage/managelist/' + object._id + '">' + name + '</a>';return new Spacebars.SafeString(html);}
-				},
-				{ key: 'email', label: '邮箱', fn: Common},
-				{ key: 'tel', label: '电话', fn: Common},
-				{ key: 'updateTime', fn: Common, sortOrder: 0, sortDirection: 'descending',hidden: true},
-				{
-					key: '',
-					label: '',
-					sortable: false,
-					headerClass: 'span1',
-					fn: function (name,object) {var html = '<div class="text-right"><a class="btn btn-info" href="/manage/managelist/' + object._id + '">编辑</a><button name="delete" class="btn btn-danger" value="' + object._id  + '">删除</button></div>';return new Spacebars.SafeString(html);
-					}
+			fields: [
+			{
+				key: 'username',
+				label: '账号',
+				headerClass: '',
+				cellClass:'',
+				fn: function (name,object) {
+					var html = '<a href="/manage/managelist/' + object._id + '">' + name + '</a>';
+					return new Spacebars.SafeString(html);
 				}
+			},
+			{ key: 'email', label: '邮箱', fn: Common},
+			{ key: 'tel', label: '电话', fn: Common},
+			{ key: 'updateTime', fn: Common, sortOrder: 0, sortDirection: 'descending',hidden: true},
+			{
+				key: '',
+				label: '',
+				sortable: false,
+				headerClass: 'span1',
+				fn: function (name,object) {
+					var html = '<div class="text-right"><a class="btn btn-info" href="/manage/managelist/' + object._id + '">编辑</a><button name="delete" class="btn btn-danger" value="' + object._id  + '">删除</button></div>';
+					return new Spacebars.SafeString(html);
+				}
+			}
 			]
 		};
 	}
