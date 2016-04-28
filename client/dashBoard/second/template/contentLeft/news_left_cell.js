f4d94c71d7cbd9b2f97a7f645bef501b5d03ea99
deric0314 <deric_c@outlook.com>
2016-04-28 10:03:12 +0800

    second list cell 大小固定

--- client/dashBoard/second/template/contentLeft/news_left_cell.js
+++ client/dashBoard/second/template/contentLeft/news_left_cell.js
@@ -1,7 +1,8 @@
 Template.newsListCell.onRendered(function() {
     //截取introduce长度
     $(".content-list #introduce").each(function () {
-        var tmp = "<p>"+$(".content-list #introduce").text().substring(0, 80) + "...<p>";
-        $(".content-list #introduce").replaceWith(tmp);
+        var tmp = $(".content-list #introduce").text().substring(0, 80) + "...";
+        console.log(tmp);
+        $(".content-list #introduce").html(tmp);
     });
 });
