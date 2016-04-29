bad1424baa7f5fd4a78a09c64720f5f0b7bef971
deric0314 <deric_c@outlook.com>
2016-04-29 10:36:46 +0800

    新闻详细  introduce背景区分

--- client/dashBoard/index/template/news_grid_cell_3.js
+++ client/dashBoard/index/template/news_grid_cell_3.js
@@ -12,7 +12,7 @@ Template.newsGridCell3.helpers({
 });
 
 Template.newsGridCell3.onRendered(function(){
-	截取introduce长度
+	//截取introduce长度
 	console.log($(".card .info p").text());
 	$(".card .info p").each(function(){
 		var tmp = $(".card .info p:first").text().substring(0,20)+"...";
