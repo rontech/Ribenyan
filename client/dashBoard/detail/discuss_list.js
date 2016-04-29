688f749240aefd7c16b31fa96210334580d1b647
deric0314 <deric_c@outlook.com>
2016-04-29 16:22:49 +0800

    二级页面自适应图片居中

--- client/dashBoard/detail/discuss_list.js
+++ client/dashBoard/detail/discuss_list.js
@@ -10,7 +10,7 @@ Template.discussList.onCreated(function(){
 	 self.autorun(function() {
 		var list = getNewsPL(Template.currentData().newsID);
 		var userIDs = list.map(function(doc) { return doc.userID; });
-		console.log(userIDs);
+		// console.log(userIDs);
 		self.subscribe("userData",userIDs);
 	 });
 	
