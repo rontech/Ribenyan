3a95d1204a61c474cb167ae0d037d82bcd4bfc0c
deric0314 <deric_c@outlook.com>
2016-05-17 12:22:36 +0800

    临时

---
+++ client/dashBoard/myPage/template/listMenu.js
@@ -0,0 +1,9 @@
+Template.listMenu.rendered = function() {
+    $('input').iCheck({
+        labelHover : false,
+        cursor : true,
+        checkboxClass : 'icheckbox_square-grey',
+        radioClass : 'iradio_square-blue',
+        increaseArea : '20%'
+    })
+}
