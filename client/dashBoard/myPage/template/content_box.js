cc810facf770b1c8761e633681e6018a7e2385f5
deric0314 <deric_c@outlook.com>
2016-05-18 09:50:10 +0800

    waiton

--- client/dashBoard/myPage/template/content_box.js
+++ client/dashBoard/myPage/template/content_box.js
@@ -1,8 +1,15 @@
 Template.contentBox.rendered = function() {
     CKEDITOR.replace("content");
-    if(!this._rendered) {
-        $("input").iCheck();
-    }
+    // Meteor.subscribe("bus_type_info");
+    // var typelists = TypeInfo.find({isVaild:1}).fetch();
+    // console.log(typelists);
+    // typelists.foreach(
+    //     $("#111").append(
+    //     "<input type='hidden' name='_id'  id='_id' value=' "+ this._id +" '/> <label class='checkbox-inline'><input id=' "+ this._id +" ' name='checkbox' type='checkbox' value=' "+ this._id._str + this.name +">"+ this.name +"&nbsp;&nbsp;</label>"
+    // )    );
+
+
+    
     $('input').iCheck({
         labelHover : false,
         cursor : true,
@@ -11,3 +18,19 @@ Template.contentBox.rendered = function() {
         increaseArea : '20%'
     })
 }
+
+Template.contentBox.helpers({
+    typelists: function() {
+        return TypeInfo.find({isVaild:1}).fetch();
+    },
+    checked: function() {
+        var user = Session.get('user_info');
+        var id = new Array();
+        for(var i=0;i<user.newsblockperms.length;i++){
+            if(this._id._str === user.newsblockperms[i].typeID._str) {
+                return "checked";
+            }
+        }
+        return "";
+    }
+});
