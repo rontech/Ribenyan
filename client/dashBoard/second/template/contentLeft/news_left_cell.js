cc400cf807addd990ee5aa7c3c8d687798776d10
deric0314 <deric_c@outlook.com>
2016-05-24 14:13:02 +0800

    二级列表 简介bug修复

--- client/dashBoard/second/template/contentLeft/news_left_cell.js
+++ client/dashBoard/second/template/contentLeft/news_left_cell.js
@@ -1,9 +1,10 @@
 Template.newsListCell.onRendered(function() {
     //截取introduce长度
     $(".content-list #introduce").each(function () {
-        var tmp = $(".content-list #introduce").text().substring(0, 80) + "...";
-        console.log(tmp);
-        $(".content-list #introduce").html(tmp);
+        if(($(this).text().length)>75){
+            var tmp = $(this).text().substring(0, 75) + "...";
+        }
+        $(this).html(tmp);
     });
 });
 
