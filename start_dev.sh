1692dca80640412579bd5a0b0725b1eae61f7249
Lambert Zhang <tyouteis@gmail.com>
2016-07-08 06:53:32 +0000

    Modify start script

--- start_dev.sh
+++ start_dev.sh
@@ -1,2 +1,1 @@
-MONGO_URL=mongodb://localhost:27017/meteor
-meteor run
+MONGO_URL=mongodb://localhost:27017/meteor ROOT_URL=http://www.ribenyan.net meteor run
--- start_product.sh
+++ start_product.sh
@@ -1,3 +1,1 @@
-MONGO_URL=mongodb://localhost:27017/meteor
-ROOT_URL=http://www.ribenyan.com 
-pm2 start ./.demeteorized/bundle/main.js
+MONGO_URL=mongodb://localhost:27017/meteor ROOT_URL=http://www.ribenyan.net pm2 start ./.demeteorized/bundle/main.js
