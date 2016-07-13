21a1f5d3ed13e0304f834bc5c7257246200d7d0a
shijian.yang <shijian.yangsd@gmail.com>
2016-07-13 15:59:32 +0900

    ios config file

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'test',
+  name: 'MeteorNews',
+  description: '不同视角看日本',
+  author: 'Rontech',
+  email: 'dev－amdin@rontech.co.jp',
+  website: 'http://www.ribenyan.net'
+});
