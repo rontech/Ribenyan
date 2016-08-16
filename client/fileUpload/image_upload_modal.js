2731bd98fcbc3596d33aba8d56a42c61fc35f363
xunyangjian <shijian.yangsd@gmail.com>
2016-08-16 13:22:15 +0900

    upload progress

--- client/fileUpload/image_upload_modal.js
+++ client/fileUpload/image_upload_modal.js
@@ -3,7 +3,7 @@ window.workers = {};
 Template.imageUploadModal.events({
     'click [name=upload]': function (ev, tpl) {
         ev.preventDefault();
-        
+
         UploadFS.selectFilesMobile(function (file) {   
                 const ONE_MB = 1024 * 100;
                 let uploader = new UploadFS.Uploader({
@@ -21,10 +21,12 @@ Template.imageUploadModal.events({
                 uploader.onComplete = function (file) {
                     // console.log(file.name + ' upload completed');
                     addRow(file._id);
+                    NProgress.done();
                 };
                 uploader.onCreate = function (file) {
                     // console.log(file.name + ' created');
                     workers[file._id] = this;
+                    NProgress.start();
                 };
                 uploader.onError = function (err, file) {
                     // console.error(file.name + ' could not be uploaded', err);
@@ -37,6 +39,7 @@ Template.imageUploadModal.events({
                         + "\n" + 'remaining: ' + (this.getRemainingTime() / 1000).toFixed(2) + 's'
                     );
                 };
+
                 uploader.start();
             });
     }
