0eed28a708ee4ee8d003417bcea63768cbc4fc7d
shijian.yang <shijian.yangsd@gmail.com>
2016-08-18 20:19:11 +0900

    ios app config

--- ios_run.sh
+++ ios_run.sh
@@ -1,1 +1,4 @@
-MONGO_URL=mongodb://40.115.250.137:27017/meteor meteor run ios-device
+MONGO_URL=mongodb://40.115.250.137:27017/meteor 
+# meteor run ios-de
+# meteor build ~/workspace/nodejs --server 40.115.250.137:80
+meteor build ~/workspace/nodejs --server=http://www.ribenyan.net
--- mobile-config.js
+++ mobile-config.js
@@ -1,10 +1,12 @@
 // This section sets up some basic app metadata,
 // the entire section is optional.
 App.info({
-  id: 'test',
+  id: 'com.rontech.ribenyan',
   name: 'MeteorNews',
   description: '不同视角看日本',
   author: 'Rontech',
-  email: 'dev－amdin@rontech.co.jp',
+  email: 'dev@rontech.co.jp',
   website: 'http://www.ribenyan.net'
 });
+
+App.accessRule('http://www.ribenyan.net/*');
