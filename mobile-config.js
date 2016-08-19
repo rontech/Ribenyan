2b0097257897b16a8f96b51e645031ce3930bd00
shijian.yang <shijian.yangsd@gmail.com>
2016-08-19 17:09:12 +0900

    ios icon and launch image

--- mobile-config.js
+++ mobile-config.js
@@ -9,4 +9,22 @@ App.info({
   website: 'http://www.ribenyan.net'
 });
 
+// app 图标
+App.icons({
+  // 'iphone': 'appicons/iphone.png',
+  'iphone_2x': 'appicons/iphone_2x_120.png',
+  'iphone_3x': 'appicons/iphone_3x_180.png',
+  'ipad' : 'appicons/ipad_76.png',
+  'ipad_2x' :'appicons/ipad_2x_152.png',
+  'ipad_pro' : 'appicons/ipad_pro_167.png'
+});
+
+App.launchScreens({
+	'iphone_2x': 'launchScreen/iphone2x_640X960.png',
+	'iphone5': 'launchScreen/iphone5_640X1136.png',
+	'iphone6': 'launchScreen/iphone6_750X1334.png',
+	'iphone6p_portrait': 'launchscreen/iphone6s_1204X2208.png'
+});
+
+// 服务器
 App.accessRule('http://www.ribenyan.net/*');
