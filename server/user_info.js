6bc45741e93de9bc828398e16345784f5148aca2
shijian.yang <shijian.yangsd@gmail.com>
2016-08-30 11:40:00 +0900

    edit wechat login  package

--- server/user_info.js
+++ server/user_info.js
@@ -17,35 +17,18 @@ Meteor.methods({
   // }
 });
 
+// 区分用户登录类型
 Accounts.onCreateUser(function (options, user) {
     if (options.profile) {
         user.profile = options.profile;
     }
 
-
-    // if(user.services.hasOwnProperty("webwechat") || user.services.hasOwnProperty("webwechat")){
-    //     // if(user.services.hasOwnProperty("webwechat")){
-    //     //   unionId = user.services.webwechat.unionId; 
-    //     // }else{
-    //     //   unionId = user.services.wechat.unionId;
-    //     // }
-    //     // var wechatoldUser = Meteor.users.findOne({"services.wechat.unionId" : unionId});
-    //     // var webwechatoldUser = Meteor.users.findOne({"services.webwechat.unionId" : unionId});
-    //     // if (wechatoldUser ){ 
-    //     //   wechatoldUser.services.webwechat = wechatoldUser.services.wechat;
-    //     //   return wechatoldUser;
-    //     // }else if(webwechatoldUser){
-    //     //   webwechatoldUser.services.wechat = webwechatoldUser.services.webwechat;
-    //     //   return webwechatoldUser;
-    //     // }else{
-    //     //   return user;
-    //     // }
-    //   }
-
-    if(user.services.hasOwnProperty("webwechat")){
-      user.services.wechat = user.services.webwechat;
+    if(user.services.hasOwnProperty("wechat")){
+      user.services.webwechat = user.services.wechat;
+      return user;
     }else if (user.services.hasOwnProperty("webwechat")){
       user.services.wechat = user.services.webwechat;
+      return user;
     }else {
       return user;
     }
