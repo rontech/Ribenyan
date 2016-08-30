80f58a46cdc6f80eb0f9d63deac5c4889f6ae74f
shijian.yang <shijian.yangsd@gmail.com>
2016-08-30 14:28:42 +0900

    edit web config

--- client/dashBoard/user/template/wechat/wechat_web_barcode.js
+++ client/dashBoard/user/template/wechat/wechat_web_barcode.js
@@ -1,19 +1,11 @@
 // web wechat login 
 Template.wechatWebBarCode.events({
 	"click li.js-ga-login" : function(e){//wechat login div 
-		// $("#wechat_login").toggle();
 		Session.set("counter", Session.get("counter") + 1);
-		// Meteor.loginWithWechat({
-		// loginStyle: 'redirect'
-		// //loginStyle: 'redirect'  you can use redirect for mobile web app
-		// }, function (err, res) {
-  //         if (err !== undefined)
-  //           console.log('sucess ' + res)
-  //         else
-  //           console.log('login failed ' + err)
-  //     });
-
-  		Meteor.loginWithWebWeChat(function (err, res) {
+    // 网页版使用 loginWithWebWechat
+    // App打包时使用 loginWithWechat
+    // 
+  		Meteor.loginWithWebWechat(function (err, res) {
           if (err) {
             console.log('success ' + res);
           }
@@ -21,21 +13,5 @@ Template.wechatWebBarCode.events({
             console.log('login failed ' + err);
           }
       });
-
-     //  Wechat.share({
-     //    message: {
-     //      title: "Hi, there",
-     //      media: {
-     //         type: Wechat.Type.IMAGE,
-     //         image:"http://www.ribenyan.net/ufs/files/wGLosFcv4PS5ucjv3/pokenmen%20go%20.jpg"
-     //      }
-     //    },
-     //    	scene: Wechat.Scene.TIMELINE   // share to Timeline
-	    // }, function () {
-	    //     alert("Success");
-	    // }, function (reason) {
-	    //     console.log(reason);
-	    //     alert("Failed: " + reason);
-	    // });
    }
 });
