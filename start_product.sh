b470c33828c58b9c535f113bffed8199896b2dbb
Lambert Zhang <tyouteis@gmail.com>
2016-09-14 12:47:43 +0000

    Downgrade meteor to 1.4.0.1

--- start_product.sh
+++ start_product.sh
@@ -1,1 +1,1 @@
-MONGO_URL=mongodb://localhost:27017/meteor ROOT_URL=http://www.ribenyan.net pm2 start ./.demeteorized/bundle/main.js
+MONGO_URL=mongodb://localhost:27017/meteor  PORT=3000 ROOT_URL=http://www.ribenyan.net pm2 start ./.demeteorized/bundle/main.js
