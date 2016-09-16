4f1ccfdb01046257a67ca31490712d84a0071a2f
Lambert Zhang <tyouteis@gmail.com>
2016-09-16 13:09:14 +0000

    Change app id to co.jp.rontech.ribenyan

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,7 @@
 // This section sets up some basic app metadata,
 // the entire section is optional.
 App.info({
-  id: 'com.rontech.ribenyan',
+  id: 'co.jp.rontech.ribenyan',
   name: 'MeteorNews',
   description: '不同视角看日本',
   author: 'Rontech',
