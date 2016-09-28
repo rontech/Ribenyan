03b8b74684419e0d4561c595ebbb83150a0574b8
Lambert Zhang <tyouteis@gmail.com>
2016-09-28 06:32:08 +0000

    Switch the category with swipe ope

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,56 @@
 var openTrue = true;
 $(function() {
+  $(window).on('touchstart', onTouchStart); //指が触れたか検知
+  $(window).on('touchmove', onTouchMove); //指が動いたか検知
+  $(window).on('touchend', onTouchEnd); //指が離れたか検知
+  var direction, position;
+
+  //スワイプ開始時の横方向の座標を格納
+  function onTouchStart(event) {
+    position = getPosition(event);
+    direction = ''; //一度リセットする
+  }
+
+  //スワイプの方向（left／right）を取得
+  function onTouchMove(event) {
+    if (position - getPosition(event) > 30) { // 70px以上移動しなければスワイプと判断しない
+      direction = 'left'; //左と検知
+    } else if (position - getPosition(event) < -30){  // 70px以上移動しなければスワイプと判断しない
+      direction = 'right'; //右と検知
+    }
+  }
+
+  function onTouchEnd(event) {
+    var current = $("#category_list li.on");
+    var index = $("#category_list li").index(current);
+    current.removeClass("on");
+    if (direction == 'right') {
+      if (index == 0) {
+        var last = $("#category_list li:last");
+        last.addClass("on");
+        last.children("a").click();
+      } else {
+        var prev = current.prev("li");
+        prev.addClass("on");
+        prev.children("a").click();
+      }
+    } else if (direction == 'left'){
+      if (index == 4) {
+        var first = $("#category_list li:first");
+        first.addClass("on");
+        first.children("a").click();
+      } else {
+        var next = current.next("li");
+        next.addClass("on");
+        next.children("a").click();
+      }
+    }
+  }
+
+  //横方向の座標を取得
+  function getPosition(event) {
+    return event.originalEvent.touches[0].pageX;
+  }
 					$('body').removeClass('with-panel-left-cover');
 					$('.respBox').removeClass('on');
 					openTrue = true;
